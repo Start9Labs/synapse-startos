20dd3c982d4f1321d5d197070d6ee30dad706228
Chris Guida <chrisguida@gmail.com>
2021-04-13 22:53:39 -0600

    it works

--- Makefile
+++ Makefile
@@ -15,12 +15,12 @@ synapse.s9pk: manifest.yaml config_spec.yaml config_rules.yaml image.tar instruc
 instructions.md: README.md
 	cp README.md instructions.md
 
-image.tar: Dockerfile docker_entrypoint.sh element-web/webapp base-image.tar
-	docker load < base-image.tar
+image.tar: Dockerfile docker_entrypoint.sh element-web/webapp #base-image.tar
+	# docker load < base-image.tar
 	DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --tag start9/synapse --platform=linux/arm/v7 -o type=docker,dest=image.tar .
 
-base-image.tar: $(SYNAPSE_SRC) synapse/docker/Dockerfile
-	DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build -f synapse/docker/Dockerfile --tag matrixdotorg/synapse:v1.31.0 --platform=linux/arm/v7 -o type=docker,dest=base-image.tar ./synapse
+# base-image.tar: $(SYNAPSE_SRC) synapse/docker/Dockerfile
+# 	DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build -f synapse/docker/Dockerfile --tag matrixdotorg/synapse:v1.31.0 --platform=linux/arm/v7 -o type=docker,dest=base-image.tar ./synapse
 
 element-web/webapp: element-web/node_modules $(ELEMENT_SRC) element-web/config.json
 	NODE_OPTIONS=--max-old-space-size=2048 npm --prefix element-web run build
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,7 +6,8 @@ export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 
 if ! [ -f /data/homeserver.yaml ]; then
     SYNAPSE_SERVER_NAME=$TOR_ADDRESS SYNAPSE_REPORT_STATS=yes /start.py generate
-    yq e -i ".federation_certificate_verification_whitelist.0 = \"*.onion\"" /data/homeserver.yaml
+    yq e -i ".federation_certificate_verification_whitelist[0] = \"*.onion\"" /data/homeserver.yaml
+    yq e -i ".listeners[0].bind_addresses = [\"127.0.0.1\"]" /data/homeserver.yaml
 fi
 
 if [ "$(yq e ".enable-registration" /data/start9/config.yaml)" = "true" ]; then
@@ -19,7 +20,7 @@ cat /var/www/config.json | jq ".default_server_config[\"m.homeserver\"].base_url
 cat /var/www/config.json | jq ".default_server_config[\"m.homeserver\"].server_name = \"${TOR_ADDRESS}\"" > /var/www/config.json.tmp && mv /var/www/config.json.tmp /var/www/config.json
 
 echo "" > /etc/nginx/conf.d/default.conf
-cat >> /etc/nginx/conf.d/default.conf <<EOT
+cat >> /etc/nginx/conf.d/default.conf <<"EOT"
 server_names_hash_bucket_size 128;
 server {
     listen 80;
@@ -29,10 +30,17 @@ server {
     ssl_certificate_key /data/key.pem;
 EOT
 echo "    server_name ${TOR_ADDRESS};" >> /etc/nginx/conf.d/default.conf
-cat >> /etc/nginx/conf.d/default.conf <<EOT
+cat >> /etc/nginx/conf.d/default.conf <<"EOT"
     root /var/www;
-    location /_matrix/ {
-        proxy_pass http://localhost:8008/_matrix/;
+    location ~* ^(\/_matrix|\/_synapse\/client) {
+        proxy_pass http://localhost:8008;
+        proxy_set_header X-Forwarded-For $remote_addr;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host $host;
+
+        # Nginx by default only allows file uploads up to 1M in size
+        # Increase client_max_body_size to match max_upload_size defined in homeserver.yaml
+        client_max_body_size 50M;
     }
 }
 EOT
