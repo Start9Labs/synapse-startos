6c0a41ef0463a1d36c0c3eeb99aa5c8b645b8929
Mariusz Kogen <k0gen@pm.me>
2023-01-10 13:07:38 +0100

    Upgrade to version 1.74.0 (#46)
    * mod.ts version update
    * switching to the 1.74 from upstream
    * Double timeout patching
    * Removing old dependencies
    * notes update and migration bump
    * .gitignore update
    * smarter Makefile

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,5 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping( {}, "1.63.1.1" );
-
+    .fromMapping( {}, "1.74.0" );
