0d7e476fb98adee91022879c2d79dad883d6b0ce
Matt Hill <matthewonthemoon@gmail.com>
2023-02-16 16:58:19 -0700

    disable federation health check if federation disabled

--- check-federation.sh
+++ check-federation.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+FEDERATION=$(yq e '.federation' /data/start9/config.yaml)
+
+if [$FEDERATION == 'false']; then
+    exit 59
+fi
+
 DURATION=$(</dev/stdin)
 if (($DURATION <= 30000 )); then
     exit 60
