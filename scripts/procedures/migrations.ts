96186c44f3cc8a551372121905691dddbe62bf74
Mariusz Kogen <k0gen@pm.me>
2023-10-10 15:42:24 +0200

    bump version, update release notes

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -32,4 +32,4 @@ export const migration: T.ExpectedExports.migration = compat.migrations
                 { version: "1.75.0", type: "down" },
             ),
       },
-    }, "1.93.0" );
+    }, "1.94.0" );
