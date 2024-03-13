86c5f497a2c3107e3d3b4efd08ba1709016ee5a4
Mariusz Kogen <k0gen@pm.me>
2024-03-13 06:02:54 +0100

    Version bump

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -32,4 +32,4 @@ export const migration: T.ExpectedExports.migration = compat.migrations
                 { version: "1.75.0", type: "down" },
             ),
       },
-    }, "1.101.0" );
+    }, "1.98.0" );
