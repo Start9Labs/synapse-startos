ba7b1d8c44642c87eb53e7f3025774ec78b71ce7
Mariusz Kogen <k0gen@pm.me>
2025-01-07 16:40:27 +0100

    update migrations

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -32,4 +32,4 @@ export const migration: T.ExpectedExports.migration = compat.migrations
                 { version: "1.75.0", type: "down" },
             ),
       },
-    }, "1.98.0" );
+    }, "1.121.1" );
