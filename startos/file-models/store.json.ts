1653fe132913e74372e694364636ced90f7ec983
Matt Hill <mattnine@protonmail.com>
2025-05-15 20:10:31 -0600

    delete unused

--- startos/file-models/store.json.ts
+++ startos/file-models/store.json.ts
@@ -1,12 +1,11 @@
 import { matches, FileHelper } from '@start9labs/start-sdk'
 import { sdk } from '../sdk'
 
-const { object, boolean, oneOf, literal, string, natural } = matches
+const { object, boolean } = matches
 
 const shape = object({
   adminUserCreated: boolean.onMismatch(true),
   serverStarted: boolean.onMismatch(true),
-  // Have the sdk export a ts-matches type for smtp
   smtp: sdk.inputSpecConstants.smtpInputSpec.validator.onMismatch({
     selection: 'disabled',
     value: {},
