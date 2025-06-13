b1de7dc17bf0c10d3bd7880678f97308d337737d
Mariusz Kogen <k0gen@pm.me>
2025-06-13 18:58:04 +0200

    beta 29

--- startos/main.ts
+++ startos/main.ts
@@ -1,5 +1,4 @@
 import { sdk } from './sdk'
-import { T } from '@start9labs/start-sdk'
 import { adminPort, homeserverPort, nginxPort } from './utils'
 import { homeserverYaml } from './fileModels/homeserver.yml'
 import { homeserverLogConfig } from './fileModels/homeserver.log.config'
@@ -123,20 +122,13 @@ server {
   )
 
   /**
-   * ======================== Additional Health Checks (optional) ========================
-   *
-   * In this section, we define *additional* health checks beyond those included with each daemon (below).
-   */
-  const additionalChecks: T.HealthCheck[] = []
-
-  /**
    * ======================== Daemons ========================
    *
    * In this section, we create one or more daemons that define the service runtime.
    *
    * Each daemon defines its own health check, which can optionally be exposed to the user.
    */
-  return sdk.Daemons.of(effects, started, additionalChecks)
+  return sdk.Daemons.of(effects, started)
     .addDaemon('synapse', {
       subcontainer,
       exec: { command: ['/start.py'] },
